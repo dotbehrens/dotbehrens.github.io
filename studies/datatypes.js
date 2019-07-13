185d6a40e6ca03161f3dda59adf1ba445c204178
Ubuntu <ubuntu@ip-172-31-90-188.ec2.internal>
2019-07-13 03:09:58 +0000

    try 6

---
+++ studies/datatypes.js
@@ -0,0 +1,28 @@
+/**
+ * DATATYPES:
+ * 
+ * 0.
+ * 
+ * 
+ *Primitive Datatypes
+ * 
+ * Number: numeric data 
+ *  Ex. 5
+ * String: character data
+ *  Ex. 'String5432!'
+ * Boolean: true or false
+ * NaN: not a number
+ * undefined: no value, uninitialized
+ * null: no value, nullified by programer
+ * 
+ * Complex Datatypes: hold other values and can be infinate in size
+ * 
+ * Object: Can contain strings, numbers, functions, arrays or other objects. Contained in key:value pairs, wraped in curly brackets {}
+ * Key is always a string. 
+ *  Ex. { Name : Dot,
+ *        Number of Pets : 5,
+ *        Names of Pets : [Maggie, Orbit, Yogi, Tribble, Padfoot]}
+ * Array: 
+ *  Ex. [ 'String , 5, ['string', 5, 6,]
+ * Function:
+ *  Ex.
